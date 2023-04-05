f2bee83380cd714e1734d4839958e32d414ee096
Samuel Ferracini <samuelferracini@Samuels-MacBook-Pro.local>
2023-04-05 18:57:28 +0930

    feat: create beach model and insert it on database

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -2,10 +2,24 @@ import { Request, Response } from 'express'
 
 import { Controller, Post } from '@overnightjs/core'
 
+import { Beach } from '../models/beach'
+import mongoose from 'mongoose'
+
 @Controller('beaches')
 export class BeachesController {
   @Post('')
   public async create(req: Request, res: Response) {
-    return res.status(201).send(req.body)
+    try {
+      const beach = new Beach(req.body)
+
+      const result = await beach.save()
+
+      return res.status(201).send(result)
+    } catch (error) {
+      if (error instanceof mongoose.Error.ValidationError)
+        return res.status(422).send({ error: error.message })
+
+      return res.status(500).send({ error: 'Internal server error' })
+    }
   }
 }
